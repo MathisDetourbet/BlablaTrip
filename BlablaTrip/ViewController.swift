e8013e0a996ae92d62f41b74cedfd1a7b70a9182
Mathis Detourbet <mathis.detourbet@gmail.com>
2020-02-19 09:16:03 +0100

    Initial Commit

---
+++ BlablaTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlablaTrip
+//
+//  Created by Mathis Detourbet on 19/2/20.
+//  Copyright © 2020 Mathis Detourbet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
